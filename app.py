e3cc21c672ffdf8061b9ef20faed3a73d669d0c6
mhbaji <hilal@abarobotics.com>
2022-11-17 17:15:12 +0700

    -

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-import cv2
+# import cv2
 
 app = Flask(__name__)
 
@@ -7,29 +7,29 @@ app = Flask(__name__)
 # def index():
 #     return "Hallo"
 
-def gen_frames():  
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            continue
-        try:
-            frame = cv2.resize(frame, (640, 480))
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
-        except:
-            pass
-    cap.release()
+# def gen_frames():  
+#     cap = cv2.VideoCapture(0)
+#     while True:
+#         success, frame = cap.read()
+#         if not success:
+#             continue
+#         try:
+#             frame = cv2.resize(frame, (640, 480))
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                     b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n') 
+#         except:
+#             pass
+#     cap.release()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     # app.run(debug=True, host="0.0.0.0", port=5000)
