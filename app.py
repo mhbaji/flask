7806c5eae7efc6b85d5e03d33a1902eaed6c842f
mhbaji <hilal@abarobotics.com>
2022-11-17 17:13:21 +0700

    -

--- app.py
+++ app.py
@@ -32,5 +32,5 @@ def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
-    # app.run(debug=True)
+    # app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
